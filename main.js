fffda2a070c3db6c2cab44f93d3b3f1913671cc5
harshpwhitehat <103309268+harshpwhitehat@users.noreply.github.com>
2023-03-04 12:33:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ console.log('ml5 version:', ml5.version);
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/oNAPiycWL/model.json',modelLoaded)
 
 function modelLoaded(){
-    console.log(model_is_loaded);
+    console.log("model_is_loaded");
 }
 
 function check()
